56574a92df5a92d915489f631e86d6d24ace0a07
misiko <misiko.eng.alx@gmail.com>
2023-02-21 22:33:09 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print a line using printf
+ *
+ * Return: End of program
+ */
+
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
