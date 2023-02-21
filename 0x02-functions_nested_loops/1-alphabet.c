eebb1c384fe0869220094bc5c5c9c89312185947
misiko <misiko.eng.alx@gmail.com>
2023-02-22 00:56:01 +0300

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Function to print alphabet
+ *
+ * Return: Exit program
+ */
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar (ch);
+}
+_putchar('\n');
+}
