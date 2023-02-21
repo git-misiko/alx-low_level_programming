2cff91920689205c323cd1aba198ef5559f13903
misiko <misiko.eng.alx@gmail.com>
2023-02-21 22:31:34 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - ENtry point
+ *
+ * Description: This is a longer description.
+ * Calling the function puts
+ * Return: End the program in main
+ */
+int main(void)
+{
+/*Initializing strings*/
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+/*Writing to stdout*/
+puts(str);
+return (0);
+}
