38afcdbbd27196cc9cf27003b41f8c625e8c14cd
misiko <misiko.eng.alx@gmail.com>
2023-02-23 00:37:46 +0300

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that prints
+ * the last digit of a number
+ * @a: passed argument
+ * @b: passed argument
+ *
+ * Return: end program.
+ */
+
+int add(int a, int b)
+{
+return (a+b);
+}
